2f0a05ed3ee19ce2335706e9fbabb97bce3cbaca
TWM Linux - Gabriel <90172365+GabrielTWMlinux@users.noreply.github.com>
2022-05-30 00:26:33 -0300

    Add files via upload

--- themes/TokyoNight/theme.lua
+++ themes/TokyoNight/theme.lua
@@ -14,21 +14,20 @@ local theme = {}
 theme.font          = "Font Awesome 9"
 
 theme.bg_normal     = "#111118"
-theme.bg_focus      = "#111118"
+theme.bg_focus      = "#203a51"
 theme.bg_urgent     = "#593131"
 theme.bg_minimize   = "#000000"
 theme.bg_systray    = theme.bg_normal
 
 theme.fg_normal     = "#ECEFF4"
-theme.fg_focus      = "#345e81"
+theme.fg_focus      = "#ffffff"
 theme.fg_urgent     = "#CC9393"
 theme.fg_minimize   = "#ffffff"
-theme.taglist_font  = "Nerd Font 15"
 
 theme.useless_gap   = dpi(0)
 theme.border_width  = dpi(1)
 theme.border_normal = "#000000"
-theme.border_focus  = "#ffffff"
+theme.border_focus  = "#777777"
 theme.border_marked = "#91231c"
 
 -- There are other variable sets
@@ -42,8 +41,8 @@ theme.border_marked = "#91231c"
 -- prompt_[fg|bg|fg_cursor|bg_cursor|font]
 -- hotkeys_[bg|fg|border_width|border_color|shape|opacity|modifiers_fg|label_bg|label_fg|group_margin|font|description_font]
 -- Example: 
-theme.tasklist_bg_focus  = "#111118"
-theme.tasklist_fg_focus  = "#6a89a2"
+theme.tasklist_bg_focus  = "#203a51"
+theme.tasklist_fg_focus  = "#ffffff"
 
 -- Generate taglist squares:
 local taglist_square_size = dpi(4)
@@ -95,7 +94,7 @@ theme.titlebar_floating_button_normal_active = themes_path.."default/titlebar/fl
 theme.titlebar_floating_button_focus_active  = themes_path.."default/titlebar/floating_focus_active.png"
 
 theme.titlebar_maximized_button_normal_inactive = themes_path.."default/titlebar/maximized_normal_inactive.png"
-theme.titlebar_maximized_button_focus_inactiv  = themes_path.."default/titlebar/mmized_focus_inactive.png"
+theme.titlebar_maximized_button_focus_inactiv = themes_path.."default/titlebar/mmized_focus_inactive.png"
 theme.titlebar_maximized_button_normal_active = themes_path.."default/titlebar/maximized_normal_active.png"
 theme.titlebar_maximized_button_focus_active  = themes_path.."default/titlebar/maximized_focus_active.png"
 
@@ -118,7 +117,7 @@ theme.layout_cornernw = themes_path.."default/layouts/cornernww.png"
 theme.layout_cornerne = themes_path.."default/layouts/cornernew.png"
 theme.layout_cornersw = themes_path.."default/layouts/cornersww.png"
 theme.layout_cornerse = themes_path.."default/layouts/cornersew.png"
-theme.tasklist_disable_icon                    = true
+theme.tasklist_disable_icon                    = false
 
 -- Generate Awesome icon:
 theme.awesome_icon = theme_assets.awesome_icon(
